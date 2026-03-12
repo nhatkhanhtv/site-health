d0edab37e71cfd98742696fb4722a9037a14d934
Khanh - Linux Mint <nhatkhanhtv@gmail.com>
2026-03-12 16:28:16 +0700

    check ssl daily

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
 
-Schedule::command(SiteSSLCheck::class)->everyTwoMinutes()->appendOutputTo(storage_path('logs/ssl-check.log'));
+Schedule::command(SiteSSLCheck::class)->dailyAt('1:00')->appendOutputTo(storage_path('logs/ssl-check.log'));
 Schedule::command(UptimeCheck::class)->everyThirtySeconds()->appendOutputTo(storage_path('logs/uptime-check.log'));
